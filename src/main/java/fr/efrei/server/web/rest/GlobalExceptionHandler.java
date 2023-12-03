33fa380b2313e9e3a7c80511dd092e8cc5e131ad
Matthew <matthew.david@efrei.net>
2023-12-03 01:37:46 +0100

    #7 Adding an error handling for URL that are not specified

---
+++ src/main/java/fr/efrei/server/web/rest/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package fr.efrei.server.web.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleAllExceptions(Exception e) {
+        //return new ResponseEntity<>("An error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>("This page has not been referenced. Please verify the URL entered.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
