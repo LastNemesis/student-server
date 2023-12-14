6f8ff3a13d7da59a02303b032e15a5bcbc1542f7
Matthew <matthew.david@efrei.net>
2023-12-14 11:14:33 +0100

    #16 Adding the grade, course and student files

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 -- Dropping tables                          (first step)
-DROP TABLE IF EXISTS course;
 DROP TABLE IF EXISTS grade;
+DROP TABLE IF EXISTS course;
 DROP TABLE IF EXISTS teacher;
 DROP TABLE IF EXISTS student;
 
 -- Dropping sequences                       (second step)
-DROP SEQUENCE IF EXISTS course_sequence;
 DROP SEQUENCE IF EXISTS grade_sequence;
+DROP SEQUENCE IF EXISTS course_sequence;
 DROP SEQUENCE IF EXISTS teacher_sequence;
 DROP SEQUENCE IF EXISTS student_sequence;
 
 -- Creating sequences                       (third step)
-CREATE SEQUENCE course_sequence START WITH 0 INCREMENT BY 1;
 CREATE SEQUENCE grade_sequence START WITH 0 INCREMENT BY 1;
+CREATE SEQUENCE course_sequence START WITH 0 INCREMENT BY 1;
 CREATE SEQUENCE teacher_sequence START WITH 0 INCREMENT BY 1;
 CREATE SEQUENCE student_sequence START WITH 0 INCREMENT BY 1;
 
@@ -26,19 +26,22 @@ CREATE TABLE IF NOT EXISTS student (
 -- -- TEACHER
 CREATE TABLE IF NOT EXISTS teacher (
     id INT DEFAULT NEXT VALUE FOR teacher_sequence PRIMARY KEY,
-    name VARCHAR(100),
-);
--- -- GRADE
-CREATE TABLE IF NOT EXISTS grade (
-    id INT DEFAULT NEXT VALUE FOR grade_sequence PRIMARY KEY,
-    type VARCHAR(100),
-    FOREIGN KEY (student_id) REFERENCES student(id),
-    FOREIGN KEY (course_id) REFERENCES course(id),
-    grade INT
+    name VARCHAR(100)
 );
 -- -- COURSE
 CREATE TABLE IF NOT EXISTS course (
     id INT DEFAULT NEXT VALUE FOR course_sequence PRIMARY KEY,
     name VARCHAR(100),
+    teacher_id INT,
     FOREIGN KEY (teacher_id) REFERENCES teacher(id)
+);
+-- -- GRADE
+CREATE TABLE IF NOT EXISTS grade (
+    grade_id INT DEFAULT NEXT VALUE FOR grade_sequence PRIMARY KEY,
+    type VARCHAR(100),
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    course_id INT,
+    FOREIGN KEY (course_id) REFERENCES course(id),
+    grade INT
 );
